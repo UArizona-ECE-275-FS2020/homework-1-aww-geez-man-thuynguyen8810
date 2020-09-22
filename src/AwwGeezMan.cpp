9bcda3d7c5f5f2013cc2e86a48b4f8b5e329cd95
thuynguyen8810 <70229553+thuynguyen8810@users.noreply.github.com>
2020-09-22 13:02:15 -0700

    Add files via upload

--- src/AwwGeezMan.cpp
+++ src/AwwGeezMan.cpp
@@ -1,48 +1,65 @@
 /*
- _____ ____ _____   ____ _____ ____  
-| ____/ ___| ____| |___ \___  | ___| 
-|  _|| |   |  _|     __) | / /|___ \ 
+ _____ ____ _____   ____ _____ ____
+| ____/ ___| ____| |___ \___  | ___|
+|  _|| |   |  _|     __) | / /|___ \
 | |__| |___| |___   / __/ / /  ___) |
-|_____\____|_____| |_____/_/  |____/ 
-                                     
- _   _                                         _      _ 
+|_____\____|_____| |_____/_/  |____/
+
+ _   _                                         _      _
 | | | | ___  _ __ ___   _____      _____  _ __| | __ / |
 | |_| |/ _ \| '_ ` _ \ / _ \ \ /\ / / _ \| '__| |/ / | |
 |  _  | (_) | | | | | |  __/\ V  V / (_) | |  |   <  | |
 |_| |_|\___/|_| |_| |_|\___| \_/\_/ \___/|_|  |_|\_\ |_|
- 
- 
-Aww Geez Man: Interdimensional Mortys.
 
+
+Aww Geez Man: Interdimensional Mortys.
 One of Morty C137's favorite phrases is "Aww Geez Man". But, in dimension Z286, there
 exists a Morty who says everything backwards.
 
+SUBMITTED BY: NGUYEN, THUY
+
 */
 
-// Include the Morty header file
-#include "Morty.hpp"
+#include <iostream>
+#include <string>
+#include "Morty.cpp"
+
+int main(int ac, char** av)
+{
+    // Parse the command line arguments. The program is executed as
+    // AwwGeezMan {start} {stop} {dimension} 
+    // AwwGeezMan {start} {stop} {step} {dimension}
+
+    if (ac != 4 and ac != 5)                    // must have 4 or 5 arguments to be valid
+    {
+        std::cout << "Error: Command line arguments are incorrect. Call program as (1) or (2)"
+            << std::endl;
+        std::cout << "(1)\t./AwwGeezMan {start} {stop} {dimension}" << std::endl;
+        std::cout << "(2)\t./AwwGeezMan {start} {stop} {step} {dimension}" << std::endl;
+        return -1;
+    }
+ 
+    if (strncmp(av[ac - 1], "C137", 4)==0)      // must use strncmp not ==, == doesn't work
+    {
+        if (ac == 5) C137::Morty(atoi(av[1]), atoi(av[2]), atoi(av[3]));    // if step there use it
+        else C137::Morty(atoi(av[1]), atoi(av[2]));                         // when no step given
+    }
+    else if (strncmp(av[ac - 1], "Z286", 4)==0) // must use strncmp not ==, == doesn't work
+    {
+        if (ac == 5) Z286::Morty(atoi(av[1]), atoi(av[2]), atoi(av[3]));    // if step there use it
+        else Z286::Morty(atoi(av[1]), atoi(av[2]));                         // when no step given
+    }
+    else
+    {
+        std::cout << "Error: unknown dimension!!" << std::endl;             // no valid dimension
+        return -1;
+    }
 
 
-int main (int ac, char** av) {
-	// Parse the command line arguments. The program is executed as
-	// ./AwwGeezMan {start} {stop} {dimension}
-	// or 
-	// ./AwwGeezMan {start} {stop} {step} {dimension}
-	if (ac != 4 and ac != 5) {
-		std::cout << "Error: Command line arguments are incorrect. Call program as (1) or (2)" 
-			<< std::endl;
-		std::cout << "(1)\t./AwwGeezMan {start} {stop} {dimension}" << std::endl;
-		std::cout << "(2)\t./AwwGeezMan {start} {stop} {step} {dimension}" << std::endl;
-		
-		return -1;
-	}
-	
-	// Parse the command line arguments
-	
-		
-	// Depending on the dimension of the arguments, call the appropriate Morty
-	// function
-	
-	
-	return 0;
+     // Parse the command line arguments
+     // Depending on the dimension of the arguments, call the appropriate Morty
+     // function
+
+    return 0;
 }
+
--- src/Morty.cpp
+++ src/Morty.cpp
@@ -1,10 +1,10 @@
-/* 
- __  __            _                                
-|  \/  | ___  _ __| |_ _   _        ___ _ __  _ __  
-| |\/| |/ _ \| '__| __| | | |      / __| '_ \| '_ \ 
+/*
+ __  __            _
+|  \/  | ___  _ __| |_ _   _        ___ _ __  _ __
+| |\/| |/ _ \| '__| __| | | |      / __| '_ \| '_ \
 | |  | | (_) | |  | |_| |_| |  _  | (__| |_) | |_) |
-|_|  |_|\___/|_|   \__|\__, | (_)  \___| .__/| .__/ 
-                       |___/           |_|   |_|  
+|_|  |_|\___/|_|   \__|\__, | (_)  \___| .__/| .__/
+                       |___/           |_|   |_|
 */
 
 // Include the Morty header file
@@ -14,8 +14,108 @@
 //          Define functions enclosed in Namespace C137 here 
 // =========================================================================== // 
 
+namespace C137      // overloaded c137 functions
+{
+    void Morty(int Start, int Stop, int Step)       // overloaded function for when there is a step
+    {
+        int i;
+        int LoopDone = 0;
 
+        std::cout << "Morty C137 says:" << std::endl;
+
+        for (i = Start; i <= Stop; i = i + Step)    // loop to go through the steps
+        {
+            std::cout << i << ": ";
+            if (i % 15 == 0)                        // if it is a multiple of 15 output Aww Geez Man 
+            {
+                std::cout << "Aww Geez Man";
+            }
+            else if (i % 5 == 0)                    // if it is a multiple of 5 output Geez 
+                std::cout << "Geez";
+            else if (i % 3 == 0)                    // if it is a multiple of 3 output Aww
+                std::cout << "Aww";
+
+            std::cout << std::endl;
+            LoopDone = 0;
+        }
+    }
+
+    void Morty(int Start, int Stop)                 // overloaded function for when there is no step
+    {
+        int i;
+        int LoopDone = 0;
+
+        std::cout << "Morty C137 says:" << std::endl;
+
+        for (i = Start; i <= Stop; i++)             // go through the steps
+        {
+            std::cout << i << ": ";
+            if (i % 15 == 0)                        // if it is a multiple of 15 output Aww Geez Man
+            {
+                std::cout << "Aww Geez Man";
+            }
+            else if (i % 5 == 0)                    // if it is a multiple of 5 output Geez 
+                std::cout << "Geez";
+            else if (i % 3 == 0)                    // if it is a multiple of 3 output Aww
+                std::cout << "Aww";         
+
+
+            std::cout << std::endl;
+            LoopDone = 0;
+        }
+    }
+}
 
 // =========================================================================== // 
 //          Define functions enclosed in Namespace Z286 here 
 // =========================================================================== // 
+
+namespace Z286      // overloaded z286 functions - reverse the Aww Geez Man (naM zeeG wwA)
+{
+    void Morty(int Start, int Stop, int Step)       // overloaded Z286 function for when there is a step
+    {
+        int i;
+        int LoopDone = 0;
+        
+        std::cout << "Morty Z286 says:" << std::endl;
+
+        for (i = Start; i <= Stop; i = i + Step)    // go through the steps
+        {
+            std::cout << i << ": ";
+            if (i % 15 == 0)                        // if it is a multiple of 15 output naM zeeG wwA 
+            {
+                std::cout << "naM zeeG wwA";
+            }
+            else if (i % 5 == 0)                    // if it is a multiple of 5 output zeeG 
+                std::cout << "zeeG";
+            else if (i % 3 == 0)                    // if it is a multiple of 3 output wwA 
+                std::cout << "wwA";
+
+            std::cout << std::endl;
+            LoopDone = 0;
+        }
+    }
+    void Morty(int Start, int Stop)                 // overloaded Z286 function for no steps
+    {
+        int i;
+        int LoopDone = 0;
+
+        std::cout << "Morty Z286 says:" << std::endl;
+
+        for (i = Start; i <= Stop; i++)
+        {
+            std::cout << i << ": ";
+            if (i % 15 == 0)                        // if it is a multiple of 15 output naM zeeG wwA 
+            {
+                std::cout << "naM zeeG wwA";
+            }
+            else if (i % 5 == 0)                    // if it is a multiple of 5 output zeeG 
+                std::cout << "zeeG";
+            else if (i % 3 == 0)                    // if it is a multiple of 3 output wwA 
+                std::cout << "wwA";
+
+            std::cout << std::endl;
+            LoopDone = 0;
+        }
+    }
+}
